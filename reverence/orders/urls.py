ffb5fdfcf5f504afc59b669b87bffd59aa0c987e
koyaware <rundelchaos@gmail.com>
2025-02-27 10:02:36 +0500

    added user profile, order processing, fixed bugs

--- reverence/orders/urls.py
+++ reverence/orders/urls.py
@@ -1,4 +1,4 @@
-from django.urls.import path
+from django.urls import path
 from .views import order_create, order_success
 
 
--- reverence/orders/views.py
+++ reverence/orders/views.py
@@ -7,29 +7,27 @@ from core.models import Size
 from django.conf import settings
 import stripe
 
-
-
 stripe.api_key = settings.STRIPE_TEST_SECRET_KEY
 
 
 @login_required(login_url='/users/login')
 def order_create(request):
     cart = Cart(request)
-    total_price = sum(item('total_price') for item in cart)
+    total_price = sum(item['total_price'] for item in cart)
 
     if request.method == 'POST':
         form = OrderForm(request.POST)
         if form.is_valid():
             order = Order(
                 user=request.user,
-                first_name=form.cleaned_data['first_name'],
-                last_name=form.cleaned_data['last_name'],
-                middle_name=form.cleaned_data['middle_name'],
-                city=form.cleaned_data['city'],
-                street=form.cleaned_data['street'],
-                house_number=form.cleaned_data['house_number'],
-                apartment_number=form.cleaned_data['apartment_number'],
-                postal_code=form.cleaned_data['postal_code'],
+                first_name=form.cleaned_data.get('first_name'),
+                last_name=form.cleaned_data.get('last_name'),
+                middle_name=form.cleaned_data.get('middle_name'),
+                city=form.cleaned_data.get('city'),
+                street=form.cleaned_data.get('street'),
+                house_number=form.cleaned_data.get('house_number'),
+                apartment_number=form.cleaned_data.get('apartment_number'),
+                postal_code=form.cleaned_data.get('postal_code'),
             )
             order.save()
 
@@ -37,7 +35,7 @@ def order_create(request):
                 size_instance = Size.objects.get(name=item['size'])
                 OrderItem.objects.create(
                     order=order,
-                    clothing_item=item['clothing_item'],
+                    clothing_item=item['item'],
                     size=size_instance,
                     quantity=item['quantity'],
                     total_price=item['total_price'],
@@ -65,17 +63,28 @@ def order_create(request):
 
                 return redirect(session.url, code=303)
             except Exception as e:
-                return render(request, 'orders/order_form.html', {'form': form, 'cart': cart, 'error': e})
-    form = OrderForm(initial={'first_name': request.user.first_name,
-                              'last_name': request.user.last_name,
-                              'middle_name': request.user.middle_name,
-                              'city': request.user.city,
-                              'street': request.user.street,
-                              'house_number': request.user.house_number,
-                              'apartment_number': request.user.apartment_number,
-                              'postal_code': request.user.postal_code})
+                return render(request, 'orders/order_form.html', {
+                    'form': form,
+                    'cart': cart,
+                    'error': str(e),
+                })
 
-    return render(request, 'orders/order_form.html', {'form': form, 'cart': cart, 'total_price': total_price})
+    form = OrderForm(initial={
+        'first_name': request.user.first_name,
+        'last_name': request.user.last_name,
+        'middle_name': request.user.middle_name,
+        'city': request.user.city,
+        'street': request.user.street,
+        'house_number': request.user.house_number,
+        'apartment_number': request.user.apartment_number,
+        'postal_code': request.user.postal_code,
+    })
+
+    return render(request, 'orders/order_form.html', {
+        'form': form,
+        'cart': cart,
+        'total_price': total_price,
+    })
 
 
 @login_required(login_url='/users/login')
