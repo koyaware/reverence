728f3e282a9f613cc9952fc1cd3c908d43d74275
koyaware <rundelchaos@gmail.com>
2025-02-27 06:25:03 +0500

    added cart

--- reverence/reverence/urls.py
+++ reverence/reverence/urls.py
@@ -6,9 +6,10 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('core.urls', namespace='core'))
+    path('cart/', include('cart.urls', namespace='cart')),
+    path('', include('core.urls', namespace='main'))
 ]
 
-
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
